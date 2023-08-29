caf53c7078539f0a2c597e3ff61e30f4421f23b0
Mussa Milanzi <viewholder74@gmail.com>
2023-08-29 07:46:28 -0700

    FIX:Task

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,37 +1,69 @@
 #include "lists.h"
 
+/**
+ * free_listp - frees a linked list
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
+void free_listp(listp_t **head)
+{
+	listp_t *temp;
+	listp_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
+ * print_listint_safe - prints a linked list.
+ * @head: head of a list.
+ *
+ * Return: number of nodes in the list.
+ */
 size_t print_listint_safe(const listint_t *head)
 {
-    const listint_t *slow = head, *fast = head;
-    size_t count = 0;
+	size_t nnodes = 0;
+	listp_t *hptr, *new, *add;
 
-    while (fast && fast->next) {
-        slow = slow->next;
-        fast = fast->next->next;
+	hptr = NULL;
+	while (head != NULL)
+	{
+		new = malloc(sizeof(listp_t));
 
-        if (slow == fast) {
-            printf("%d", slow->n);
-            slow = slow->next;
-            count = 1;
-            while (slow != fast) {
-                printf(", %d", slow->n);
-                slow = slow->next;
-                count++;
-            }
-            printf(" -> [%d]", fast->n);
-            break;
-        }
+		if (new == NULL)
+			exit(98);
 
-        printf("%d", slow->n);
-        if (fast->next) {
-            printf(", ");
-        }
+		new->p = (void *)head;
+		new->next = hptr;
+		hptr = new;
 
-        slow = slow->next;
-        fast = fast->next;
-        count++;
-    }
+		add = hptr;
 
-    printf("\n");
-    return count;
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (head == add->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_listp(&hptr);
+				return (nnodes);
+			}
+		}
+
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		nnodes++;
+	}
+
+	free_listp(&hptr);
+	return (nnodes);
 }
