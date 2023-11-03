64a5e619926057c492104a9413e0fc87ccca1934
Mussa Milanzi <viewholder74@gmail.com>
2023-11-03 13:46:20 -0700

    hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table;
+	hash_table_t *new_node;
 	unsigned long int i;
 
 	new_node = malloc(sizeof(hash_table_t));
